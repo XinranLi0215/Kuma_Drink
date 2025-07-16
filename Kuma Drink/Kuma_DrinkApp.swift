c747f07e94fd515c8ea08d811c81127d80e0d127
XinranLi0215 <xinranli2024@u.northwestern.edu>
2025-07-16 15:43:56 -0700

    Add files via upload

---
+++ Kuma Drink/Kuma_DrinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  Kuma_DrinkApp.swift
+//  Kuma Drink
+//
+//  Created by 松本夫人 on 7/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Kuma_DrinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Kuma DrinkTests/Kuma_DrinkTests.swift
@@ -0,0 +1,16 @@
+//
+//  Kuma_DrinkTests.swift
+//  Kuma DrinkTests
+//
+//  Created by 松本夫人 on 7/16/25.
+//
+
+import Testing
+
+struct Kuma_DrinkTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
